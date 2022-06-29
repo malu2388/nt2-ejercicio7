4bd6a62bed23054b7beea1392f8671b3009288db
malu2388 <malu2388@gmail.com>
2022-06-29 13:05:04 -0300

    first commit

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="src-components-usuarios">
+    <div class="jumbotron">
+      <h2>Usuarios</h2>
+      <hr />
+      <br />
+
+      <div v-if="usuarios.length" class="table-responsive">
+        <table class="table table-dark">
+          <tr>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Mail</th>
+          </tr>
+          <tr v-for="(usuario, i) in usuarios" :key="i">
+            <td>{{ usuario.nombre }}</td>
+            <td>{{ usuario.edad }}</td>
+            <td>{{ usuario.email }}</td>
+          </tr>
+        </table>
+      </div>
+      <h4 v-else class="alert alert-danger text-center">
+        No se encontraron usuarios
+      </h4>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "src-components-usuarios",
+  props: [],
+  mounted() {
+    this.getUsuariosAxios();
+  },
+  data() {
+    return {
+      url: "https://62b708ec491a19c97aed7ab6.mockapi.io/form-TP7",
+      usuarios: [],
+    };
+  },
+  methods: {
+    async getUsuariosAxios() {
+      try {
+        let { data } = await this.axios(this.url);
+        this.usuarios = data;
+      } catch (error) {
+        console.error("Error Axios", error);
+      }
+    },
+  },
+
+  computed: {},
+};
+</script>
+
+<style scoped lang="css">
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import { router } from './router'
+import './form'
+import './axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+    router,
+}).$mount('#app')
